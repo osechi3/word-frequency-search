75f870d59ab1fd72d43242ff89ad0f030c76a063
osechi3 <holypoetwrk@gmail.com>
2020-12-29 20:42:44 +0300

    Setting click and keyup events

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import './styles/app.scss'
 import 'normalize-scss'
 
-console.log('Hello World!')
+const searchInput = document.querySelector('.search__input')
+const searchButton = document.querySelector('.search__search-btn')
+
+searchInput.addEventListener('keyup', searchWithEnter.bind(this))
+searchButton.addEventListener('click', search)
+
+function search () {
+  console.log(searchInput.value)
+  searchInput.value = ''
+}
+
+function searchWithEnter (e) {
+  if (e.key === 'Enter') {
+    searchButton.click()
+  }
+}
